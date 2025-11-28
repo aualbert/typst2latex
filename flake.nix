208023288a12e7750d9bd39c9fd5fbfd9004f016
aalbert <augustin.albert@bleu-azure.fr>
2025-11-28 09:39:34 +0100

    add meta

--- flake.nix
+++ flake.nix
@@ -1,31 +1,20 @@
 {
-  description = "Rust devshell with pandoc";
-
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    rust-overlay.url = "github:oxalica/rust-overlay";
+    nixpkgs.url = "nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { nixpkgs, rust-overlay, flake-utils, ... }:
+  outputs = { rust-overlay, flake-utils, nixpkgs, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         overlays = [ (import rust-overlay) ];
         pkgs = import nixpkgs {
           inherit system overlays;
         };
-
-        typst2latex = pkgs.rustPlatform.buildRustPackage {
-          pname = "typst2latex";
-          version = "0.1.0";
-          src = ./.;
-          cargoLock = {
-            lockFile = ./Cargo.lock;
-          };
-          nativeBuildInputs = [ pkgs.pkg-config ];
-          buildInputs = [ pkgs.pandoc ];
-        };
-
       in
       {
         devShells.default = with pkgs; mkShell {
@@ -37,8 +26,24 @@
           ];
         };
 
-        packages = { default = typst2latex; };
-
+        packages = {
+          default =
+            pkgs.rustPlatform.buildRustPackage {
+              pname = "typst2latex";
+              version = "0.1.0";
+              src = ./.;
+              cargoLock = {
+                lockFile = ./Cargo.lock;
+                allowBuiltinFetchGit = true;
+              };
+              meta = with pkgs.lib; {
+                description = "An oppiniated converter from unequivocal-ams typst documents to latex.";
+                homepage = "https://github.com/aualbert/typst2latex";
+                license = licenses.mit;
+                maintainers = [ ];
+              };
+            };
+        };
       }
     );
 }
